1ad8929e15a58492a2d3ddd26b51afb9eb9c2903
vamsi1665 <vamsichinnu.77@gmail.com>
2025-08-13 11:20:05 +0530

    New Python selenium Test Project

---
+++ pageObjects/LoginPage.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+    textbox_username_id="Email"
+    textbox_password_id="Password"
+    button_login_xpath="//button[@class='button-1 login-button']"
+    link_logout_linktext="Logout"
+
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def setUsername(self,username):
+        self.driver.find_element(By.ID,self.textbox_username_id).clear()
+        self.driver.find_element(By.ID,self.textbox_username_id).send_keys(username)
+
+    def setPassword(self,password):
+        self.driver.find_element(By.ID,self.textbox_password_id).clear()
+        self.driver.find_element(By.ID,self.textbox_password_id).send_keys(password)
+
+    def clicklogin(self):
+        self.driver.find_element(By.XPATH,self.button_login_xpath).click()
+
+    def clicklogout(self):
+        self.driver.find_element(By.LINK_TEXT,self.link_logout_linktext).click()
+
+
+
+
